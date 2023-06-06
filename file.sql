8d5ecfc29bce4ef403a58b4e372cb47d57e1891d
Pritam750 <131663628+Pritam750@users.noreply.github.com>
2023-06-06 16:00:07 +0530

    Update file.sql

--- file.sql
+++ file.sql
@@ -1,1 +1,10 @@
-SELECT * FROM library_management_system.members;
+create database PRITAM;
+use database PRITAM;
+ CREATE TABLE Employee (employee_id INT PRIMARY KEY,
+        first_name VARCHAR(50),
+        last_name VARCHAR(50),
+        email VARCHAR(100),
+        phone_number VARCHAR(20),
+        hire_date DATE,
+        salary DECIMAL(10,2),
+        department_id INT );
